d968c436e356bd8a9e6e87425b26d36b27d869de
Jennifer <z5118780@unsw.edu.au>
2018-10-17 15:25:11 +1100

    more sign up code

--- app/src/main/java/com/example/jennwong/smartup/MainActivity.java
+++ app/src/main/java/com/example/jennwong/smartup/MainActivity.java
@@ -20,7 +20,7 @@ import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    MaterialEditText edtNewUserName,edtNewPassword,edtNewEmail; //for sign up
+    MaterialEditText edtNewUserName, edtNewPassword, edtNewEmail; //for sign up
     MaterialEditText edtUser, edtPassword; //for sign in;
 
 
@@ -30,13 +30,6 @@ public class MainActivity extends AppCompatActivity {
     DatabaseReference users;
 
 
-
-
-
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,14 +37,14 @@ public class MainActivity extends AppCompatActivity {
 
         //Firebase
 
-        database= FirebaseDatabase.getInstance();
-        users=database.getReference("Users");
+        database = FirebaseDatabase.getInstance();
+        users = database.getReference("Users");
 
-        edtUser=(MaterialEditText)findViewById(R.id.edtUser);
-        edtPassword=(MaterialEditText)findViewById(R.id.edtPassword);
+        edtUser = (MaterialEditText) findViewById(R.id.edtUser);
+        edtPassword = (MaterialEditText) findViewById(R.id.edtPassword);
 
-        btn_sign_in=(Button)findViewById(R.id.btn_sign_in);
-        btn_sign_up=(Button)findViewById(R.id.btn_sign_up);
+        btn_sign_in = (Button) findViewById(R.id.btn_sign_in);
+        btn_sign_up = (Button) findViewById(R.id.btn_sign_up);
 
         btn_sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,18 +54,60 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signin(edtUser.getText().toString(),edtPassword.getText().toString());
 
+            }
+        });
 
     }
 
-    private void showSignUpDialog(){
+    private void signin(final String user, final String pwd){
+        users.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(user).exists())
+                {
+                if(!user.isEmpty())
+                {
+                    User login =dataSnapshot.child(user).getValue(User.class);
+                        if(login.getPassword().equals(pwd))
+                            Toast.makeText(MainActivity.this,"Login Ok!",Toast.LENGTH_SHORT).show();
+                        else Toast.makeText(MainActivity.this, "Wrong Password", Toast.LENGTH_SHORT).show();
+                }
+                        else
+                {
+                    Toast.makeText(MainActivity.this,"Please enter your user name", Toast.LENGTH_SHORT).show();
+                }
+
+                }
+
+                else {
+                    Toast.makeText(MainActivity.this, "User does not exist!", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void showSignUpDialog() {
         //AlertDialog.Builder_alertDialog = new AlertDialog.Builder(MainActivity.this);
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
         alertDialog.setTitle("Sign Up");
         alertDialog.setMessage("Please fill in the full information");
 
         LayoutInflater inflater = this.getLayoutInflater();
-        View signup_layout = inflater.inflate(R.layout.signup_layout,null);
+        View signup_layout = inflater.inflate(R.layout.signup_layout, null);
 
         edtNewUserName = (MaterialEditText) signup_layout.findViewById(R.id.edtNewUserName);
         edtNewEmail = (MaterialEditText) signup_layout.findViewById(R.id.edtNewEmail);
@@ -85,33 +120,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.dismiss();
-                }
+            }
         });
 
         alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-               User user = new User(edtNewUserName.getText().toString(),edtNewPassword.getText().toString(),edtNewEmail.getText().toString());
+                final User user = new User(edtNewUserName.getText().toString(), edtNewPassword.getText().toString(), edtNewEmail.getText().toString());
 
-               users.addListenerForSingleValueEvent(new ValueEventListener() {
-                   @Override
-                   public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                       if(dataSnapshot.child(user.getUser_name()).exists())
-                           Toast.makeText(MainActivity.this,"User already exists!", Toast.LENGTH_SHORT).show();
-                       
+                users.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.child(user.getUser_name()).exists())
+                            Toast.makeText(MainActivity.this, "User already exists!", Toast.LENGTH_SHORT).show();
+                        else {
+                            users.child(user.getUser_name())
+                                    .setValue(user);
+                            Toast.makeText(MainActivity.this, "User registration success", Toast.LENGTH_SHORT).show();
+                        }
 
-                   }
+                    }
 
-                   @Override
-                   public void onCancelled(@NonNull DatabaseError databaseError) {
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                   }
-               });
+                    }
+                });
+
+                dialogInterface.dismiss();
+
             }
         });
+
+        alertDialog.show();
 
 
     }
 
-
 }
+
