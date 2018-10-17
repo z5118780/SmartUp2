415e94837d93563989782b00d84c29ed54f6b7b6
Jennifer Wong <jennwong@Jennifers-MacBook-Pro.local>
2018-10-17 12:28:51 +1100

    Initial commit

---
+++ app/src/main/java/com/example/jennwong/smartup/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.jennwong.smartup;
+
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.jennwong.smartup.Sign_in.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialEditText edtNewUserName,edtNewPassword,edtNewEmail; //for sign up
+    MaterialEditText edtUser, edtPassword; //for sign in;
+
+
+    Button btn_sign_up, btn_sign_in;
+
+    FirebaseDatabase database;
+    DatabaseReference users;
+
+
+
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Firebase
+
+        database= FirebaseDatabase.getInstance();
+        users=database.getReference("Users");
+
+        edtUser=(MaterialEditText)findViewById(R.id.edtUser);
+        edtPassword=(MaterialEditText)findViewById(R.id.edtPassword);
+
+        btn_sign_in=(Button)findViewById(R.id.btn_sign_in);
+        btn_sign_up=(Button)findViewById(R.id.btn_sign_up);
+
+        btn_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSignUpDialog();
+
+            }
+        });
+
+
+
+    }
+
+    private void showSignUpDialog(){
+        //AlertDialog.Builder_alertDialog = new AlertDialog.Builder(MainActivity.this);
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle("Sign Up");
+        alertDialog.setMessage("Please fill in the full information");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View signup_layout = inflater.inflate(R.layout.signup_layout,null);
+
+        edtNewUserName = (MaterialEditText) signup_layout.findViewById(R.id.edtNewUserName);
+        edtNewEmail = (MaterialEditText) signup_layout.findViewById(R.id.edtNewEmail);
+        edtNewPassword = (MaterialEditText) signup_layout.findViewById(R.id.edtNewPassword);
+
+        alertDialog.setView(signup_layout);
+        alertDialog.setIcon(R.drawable.ic_account_circle_black_24dp);
+
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                }
+        });
+
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+               User user = new User(edtNewUserName.getText().toString(),edtNewPassword.getText().toString(),edtNewEmail.getText().toString());
+
+               users.addListenerForSingleValueEvent(new ValueEventListener() {
+                   @Override
+                   public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                       if(dataSnapshot.child(user.getUser_name()).exists())
+                           Toast.makeText(MainActivity.this,"User already exists!", Toast.LENGTH_SHORT).show();
+                       
+
+                   }
+
+                   @Override
+                   public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                   }
+               });
+            }
+        });
+
+
+    }
+
+
+}
